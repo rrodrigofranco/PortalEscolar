c53b5f8d92bcc26d7f24c36555c35db3e845ec3b
Rodrigo Franco <rodrigo.franco@apiki.com>
2021-12-19 11:06:08 -0300

    fix: fazendo alteracoes no banco de dados

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php 
 
-$nome_escola = "Escola Freitas";
+$nome_escola = "Escola America Ribeiro";
 $url = "http://localhost/escolar/";
-$endereco_escola = "Rua Alameda Campos, 157, Belo Horizonte";
-$telefone_escola = "(31)97527-5084";
-$email_adm = 'hvfadvocacia@gmail.com';
-$rodape_relatorios = "Desenvolvido por Hugo Vasconcelos - Portal Hugo Cursos";
-$cnpj_escola = '26.100.560/0000-50';
-$cidade_escola = 'Belo Horizonte';
+$endereco_escola = "Rua Dr. Maninho, nº 72, Centro, Entre Folhas - MG";
+$telefone_escola = "(33) 3324-6106";
+$email_adm = 'emamericaribeiro@gmail.com';
+$rodape_relatorios = "Desenvolvido por Rodrigo Franco";
+$cnpj_escola = '66.229.626/0001-82';
+$cidade_escola = 'Entre Folhas';
 
 
 //VARIAVEIS DO BANCO DE DADOS LOCAL
